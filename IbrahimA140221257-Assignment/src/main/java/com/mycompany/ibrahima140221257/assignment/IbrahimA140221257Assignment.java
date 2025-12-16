976c257a9a334bbaa1b5adfb07db889fa4a549cb
iby <iby@Ibylinux>
2025-12-16 10:33:37 +0000

    Test repo

---
+++ IbrahimA140221257-Assignment/src/main/java/com/mycompany/ibrahima140221257/assignment/IbrahimA140221257Assignment.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ibrahima140221257.assignment;
+
+/**
+ *
+ * @author iby
+ */
+public class IbrahimA140221257Assignment {
+
+    public static void main(String[] args) {
+        System.out.println("i am the smratest name alie"
+                + "");
+    }
+}
